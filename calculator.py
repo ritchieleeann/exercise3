e515686de9671a2a4c1d903ebf23d97b5ed446d8
Hackbright Student <info@hackbrightacademy.com>
2014-02-05 17:29:48 -0800

    Working calculator. Draft 1

--- calculator.py
+++ calculator.py
@@ -4,8 +4,28 @@ start = raw_input("> ")
 
 token = start.split(" ")
 operator = token[0]
-var1 = int(token[1])
-var2 = int(token[2])
 
 if operator == "+":
-    print add(var1 , var2)
+    print add(int(token[1]) , int(token[2]))
+
+if operator == "-":
+    print subtract(int(token[1]) , int(token[2]))
+
+if operator == "*":
+    print multiply(int(token[1]) , int(token[2]))
+
+if operator == "/":
+    print divide(int(token[1]) , int(token[2]))
+
+if operator == "square":
+    print square(int(token[1]))
+
+if operator == "cube":
+    print cube(int(token[1]))
+
+if operator == "pow":
+    print power(int(token[1]) , int(token[2]))
+
+if operator == "mod":
+    print mod(int(token[1]) , int(token[2]))
+
